23165e8f05dcf52ae9bc89932de961fd62d449c0
small-white-rabbit <525307811@qq.com>
2019-10-28 23:32:35 +0800

    Update 漫画下载.py

--- 漫画下载.py
+++ 漫画下载.py
@@ -8,6 +8,7 @@
 3.0 系列图片下载
 4.0 模块化代码、增加多线程下载添加输入模块 直接输入链接开始下载
 5.0 加入位置存储及文件夹自动创建
+6.0 创建html网页，将文件名存入网页内，实现网页内查看漫画
 """
 
 import requests
@@ -15,13 +16,13 @@ from lxml import etree
 import time
 import os
 from concurrent import futures
-url_base = input('请输入起始链接：' )
+url_base = input('\n-----------------------请在下方输入起始链接-----------------------\n' )
 url = str(url_base+'/')
 headers = {
         'Referer': url,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 #保存位置
-downloadname = r"F://漫画//"
+downloadname = "F://漫画//"
 
 def download_img(src,dirnames,filename):
 
@@ -30,18 +31,20 @@ def download_img(src,dirnames,filename):
     with open(filename,'wb') as fd:             #将图片写入
         fd.write(img.content)
         fd.close()
-
+    with open('快速查看.html','a') as fdd:
+         fdd.write("<div align='center'><img src='{}'></div>".format(filename))
+         fdd.close()
 def get_page(url):
-
+    global dirnames
+    print(url)
     resp=requests.get(url,headers=headers)
     filename = url.split('/')[-1]+'.jpg'
-    print(url)
     html = etree.HTML(resp.text)
-    title = str(html.xpath('/html/head/title/text()'))
-    dirnames = title.split('- 成人H漫畫')[0]
-    delname = dirnames.replace('/', '-').replace("['", '').replace('\'', '').replace('<','').replace('>','').replace('?', '').replace('｜', '').replace("'", '').replace('"', '')
-    dirnames = os.path.join(downloadname,delname)
-
+    title = html.xpath('/html/head/title/text()')
+    for name in title:
+        dirnames = name.split('- 成人H漫畫')[0]
+        delname = dirnames.replace('/', '-').replace("['", '').replace('\'', '').replace('<','').replace('>','').replace('?', '').replace('｜', '').replace("'", '').replace('"', '')
+        dirnames = os.path.join(downloadname,delname)
     if not os.path.exists(dirnames):
            os.makedirs(dirnames)
     scrs = html.xpath('//*[@id="comic"]/@src')
@@ -60,7 +63,8 @@ def main():
         time.sleep(1)
         current_num = current_num + 1
         next_link = get_page(next_link_base+str(current_num))
-
+    if  next_link == next_link:
+        print('\n -----------------------已经下载完毕！请打开目录查看.-----------------------------')
 
 if __name__ == "__main__":
     main()
